6dd1f5567c957afc0d44fc2b5e0a4fa4e5608b40
Eric Meyer <Eric@blackbook.local>
2009-11-26 18:38:21 -1000

    Added warning message for learner_profiles that are empty

--- app/models/learner_profile.rb
+++ app/models/learner_profile.rb
@@ -2,6 +2,8 @@
 
 class LearnerProfile < ActiveRecord::Base
   
+  named_scope :no_description, :conditions=>{:description=>""}
+  
   #validates_length_of :description, :minimum=> 2, :message=>"How will the development of this trait be supported in this unit?"
   
   belongs_to :unit_planner
